253acd094e8f33df24906d0c44d0a0804da3a9ab
Dat nguyen <ntiendat.it@gmail.com>
2024-05-30 23:18:19 +0700

    update index.html

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import router from "./router";
+import CanvasJSChart from '@canvasjs/vue-charts';
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -11,4 +12,5 @@ pinia.use(piniaPluginPersistedstate)
 app.use(app);
 app.use(router);
 app.use(pinia);
+app.use(CanvasJSChart);
 app.mount("#app");
